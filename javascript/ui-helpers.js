4a06ab839f2e0591e12caf69a4e063362af4f66e
Svitlana Hryshchuk <r.svetulik@gmail.com>
2023-06-26 15:24:18 +0200

    Changed getTodoListId function from jQuery to Vanilla JS

--- javascript/ui-helpers.js
+++ javascript/ui-helpers.js
@@ -1,10 +1,11 @@
 /**
  * Retrieves To-Do list's unique indentifier for given HTML element.
- * @param {HTMLElement} $element HTML element inside To-Do list.
+ * @param {HTMLElement} element HTML element inside To-Do list.
  * @returns {Number} The unique indentifier of the To-Do list.
  */
-const getTodoListId = ($element) => {
-  return parseInt($element.data('todo-list-id'));
+const getTodoListId = (element) => {
+  const elementTodoListId = element.getAttribute('data-todo-list-id');
+  return parseInt(elementTodoListId);
 };
 
 /**
